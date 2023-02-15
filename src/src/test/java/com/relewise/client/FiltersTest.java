0fa8a0ee32d2fd267bcc8e6ce69065931e2c5df8
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2023-02-15 14:11:04 +0100

    Added Filters Test

---
+++ src/src/test/java/com/relewise/client/FiltersTest.java
@@ -0,0 +1,57 @@
+package com.relewise.client;
+
+import com.relewise.client.factory.UserFactory;
+import com.relewise.client.model.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FiltersTest extends TestBase {
+    @Test
+    void testProductAssortmentFilter() throws Exception {
+        var searcher = new Searcher(GetDatasetId(), GetApiKey());
+
+        var productSearch = ProductSearchRequest.create(
+            Language.create("en-US"),
+            Currency.create("USD"),
+            UserFactory.byTemporaryId("t-id"),
+            "integration test",
+            "1",
+            0,
+            20
+        ).setFilters(
+            FilterCollection.create(
+                ProductAssortmentFilter.create().setAssortments(10_000_000)
+            )
+        );
+
+        var response = searcher.search(productSearch);
+        assertNotNull(response);
+        assertEquals(0, response.results.length);
+    }
+
+    @Test
+    void testProductIdFilter() throws Exception {
+        var searcher = new Searcher(GetDatasetId(), GetApiKey());
+
+        var productSearch = ProductSearchRequest.create(
+            Language.create("en-US"),
+            Currency.create("USD"),
+            UserFactory.byTemporaryId("t-id"),
+            "integration test",
+            "1",
+            0,
+            20
+        ).setFilters(
+            FilterCollection.create(
+                ProductIdFilter.create().setProductIds("1")
+            )
+        );
+
+        var response = searcher.search(productSearch);
+        assertNotNull(response);
+        assertEquals(1, response.results.length);
+    }
+}
