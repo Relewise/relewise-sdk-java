f9a3258be6b256ab21033e6dbbf124d06954f3b6
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2023-02-15 14:44:00 +0100

    Added DataObjects Test

---
+++ src/src/test/java/com/relewise/client/DataObjectsTest.java
@@ -0,0 +1,48 @@
+package com.relewise.client;
+
+import com.relewise.client.factory.DataValueFactory;
+import com.relewise.client.factory.UserFactory;
+import com.relewise.client.model.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class DataObjectsTest extends TestBase {
+    @Test
+    void testDataObjectsCanBeDeserialized() throws Exception {
+        var searcher = new Searcher(GetDatasetId(), GetApiKey());
+
+        var productSearch = ProductSearchRequest.create(
+                Language.create("da-dk"),
+                Currency.create("DKK"),
+                UserFactory.anonymous(),
+                "integration test - data object",
+                "1",
+                0,
+                0
+            ).setSettings(ProductSearchSettings.create()
+                .setSelectedProductProperties(SelectedProductPropertiesSettings.create()
+                    .setAllData(true)
+                )
+            )
+            .setFilters(
+                FilterCollection.create(
+                    ProductDataFilter.create("DataObject")
+                        .setConditions(ValueConditionCollection.create()
+                            .setItems(
+                                ContainsCondition.create(DataValueFactory.create(
+                                    new Object() {
+                                        public final DataValue d = DataValueFactory.create("a");
+                                    }
+                                ))
+                            )
+                        )
+                )
+            );
+
+        var response = searcher.search(productSearch);
+        assertNotNull(response);
+        assertEquals(0, response.hits);
+    }
+}
