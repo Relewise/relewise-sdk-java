af27a1d8538bb532b4b7a52d725495624453803f
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2023-02-16 10:08:12 +0100

    Added regression test for different styles of DTO creation.

---
+++ src/src/test/java/com/relewise/client/GeneratedRequestsTest.java
@@ -0,0 +1,81 @@
+package com.relewise.client;
+
+import com.relewise.client.factory.DataValueFactory;
+import com.relewise.client.factory.UserFactory;
+import com.relewise.client.model.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeneratedRequestsTest extends TestBase {
+    @Test
+    void testTrackOrderRequestWithBuilderPatternAndCreatorMethod() throws Exception {
+        var tracker = new Tracker(GetDatasetId(), GetApiKey());
+
+        var orderRequest = TrackOrderRequest.create(
+            Order.create(
+                UserFactory.byTemporaryId("t-id"),
+                Money.create(Currency.create("DKK"), 100.0),
+                "1",
+                "1"
+            ).setChannel("Channel 1")
+        );
+
+        Executable action = () -> tracker.track(orderRequest);
+        assertDoesNotThrow(action);
+    }
+
+    /**
+     * This is a regression test to test that we can still new-up classes without the create method. Don't use this style in real scenarios.
+     *
+     * @throws Exception
+     */
+    @Test
+    void testTrackOrderRequestWithBuilderPattern() throws Exception {
+        var tracker = new Tracker(GetDatasetId(), GetApiKey());
+
+        var orderRequest = (new TrackOrderRequest())
+            .setOrder((new Order())
+                .setUser(new User().setTemporaryId("t-Id"))
+                .setSubtotal((new Money())
+                    .setAmount(100.0)
+                    .setCurrency((new Currency())
+                        .setValue("DKK")
+                    )
+                )
+                .setOrderNumber("1")
+                .setCartName("1")
+                .setChannel("Channel 1")
+            );
+
+        Executable action = () -> tracker.track(orderRequest);
+        assertDoesNotThrow(action);
+    }
+
+    /**
+     * This is a regression test to test that we can still new-up classes without the create method and set each property without the fluent style creation. Don't use this style in real scenarios.
+     *
+     * @throws Exception
+     */
+    @Test
+    void testTrackOrderRequest() throws Exception {
+        var tracker = new Tracker(GetDatasetId(), GetApiKey());
+
+        var order = new Order(
+            UserFactory.byTemporaryId("t-ID"),
+            new Money(
+                new Currency("DKK"),
+                100.0
+            ),
+            "1",
+            "1"
+        );
+        order.setChannel("Channel 1");
+
+        var orderRequest = new TrackOrderRequest(order);
+
+        Executable action = () -> tracker.track(orderRequest);
+        assertDoesNotThrow(action);
+    }
+}
