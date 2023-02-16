f895613d6720b35ba9b781be9803bccc03f22f85
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2023-02-16 08:20:04 +0100

    Added Batched Product Recommendation test.

---
+++ src/src/test/java/com/relewise/client/BatchedContentRecommendationsTest.java
@@ -0,0 +1,38 @@
+package com.relewise.client;
+
+import com.relewise.client.factory.UserFactory;
+import com.relewise.client.model.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BatchedContentRecommendationsTest extends TestBase {
+    @Test
+    void testBatchedProductRecommendations() throws Exception {
+        var recommender = new Recommender(GetDatasetId(), GetApiKey());
+
+        var contentRecommendationRequestCollection  = ContentRecommendationRequestCollection.create(
+            false,
+            PopularContentsRequest.create(
+                Language.create("en-US"),
+                Currency.create("USD"),
+                "batched integration test",
+                UserFactory.byTemporaryId("t-Id")
+            ).setSettings(ContentRecommendationRequestSettings.create().setNumberOfRecommendations(1)),
+            ContentsViewedAfterViewingContentRequest.create(
+                Language.create("en-US"),
+                Currency.create("USD"),
+                "batched integration test",
+                UserFactory.byTemporaryId("t-Id"),
+                "1"
+            ).setSettings(ContentRecommendationRequestSettings.create().setNumberOfRecommendations(1))
+        );
+
+        var response = recommender.recommend(contentRecommendationRequestCollection );
+
+        assertNotNull(response);
+        assertEquals(2, response.responses.length);
+        assertNotEquals(0, response.responses[0].recommendations.length);
+        assertNotEquals(0, response.responses[1].recommendations.length);
+    }
+}
