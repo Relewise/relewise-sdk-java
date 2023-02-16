8bd33edaab023251ffd7b44d11c904b2a12918f4
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2023-02-16 08:02:36 +0100

    Added Batched Search test.

---
+++ src/src/test/java/com/relewise/client/BatchedSearchesTest.java
@@ -0,0 +1,27 @@
+package com.relewise.client;
+
+import com.relewise.client.factory.UserFactory;
+import com.relewise.client.model.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BatchedSearchesTest extends TestBase {
+    @Test
+    void testBatchedSearch() throws Exception {
+        var searcher = new Searcher(GetDatasetId(), GetApiKey());
+
+        var searchRequestCollection = SearchRequestCollection.create(
+            SearchTermPredictionRequest.create(Language.create("en-US"), Currency.create("USD"), UserFactory.anonymous(), "integration test", "", 0),
+            ProductSearchRequest.create(Language.create("en-US"), Currency.create("USD"), UserFactory.anonymous(), "integration test", "a", 0, 0),
+            ProductCategorySearchRequest.create(Language.create("en-US"), Currency.create("USD"), UserFactory.anonymous(), "integration test", "c", 0, 0)
+        );
+
+        var response = searcher.batch(searchRequestCollection);
+
+        assertNotNull(response);
+        assertNotEquals(0, response.responses.length);
+    }
+}
