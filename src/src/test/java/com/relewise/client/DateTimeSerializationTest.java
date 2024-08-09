d52013cd0d00752908b3da81798e6c9d86a8a2e2
KSGRelewise <119578211+KSGRelewise@users.noreply.github.com>
2024-08-09 11:08:59 +0200

    Added test to ensure that it works as expected both for `OffsetDateTime` and `LocalDateTime`.

---
+++ src/src/test/java/com/relewise/client/DateTimeSerializationTest.java
@@ -0,0 +1,45 @@
+package com.relewise.client;
+
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.relewise.client.factory.UserFactory;
+import com.relewise.client.model.*;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DateTimeSerializationTest extends TestBase {
+    @Test
+    public void testSerializeOffsetDateTime() {
+        var objectMapper = JsonMapper.builder()
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .addModule(new JavaTimeModule())
+            .build();
+
+        var input = "2024-08-09T00:00:00+02:00";
+
+        var serialized = objectMapper.writeValueAsString(OffsetDateTime.parse(input));
+
+        assertEquals("\"" + input + "\"", serialized);
+    }
+
+    @Test
+    public void testSerializeLocalDateTime() {
+        var objectMapper = JsonMapper.builder()
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .addModule(new JavaTimeModule())
+            .build();
+
+        var input = "2024-08-09T00:00:00";
+
+        var serialized = objectMapper.writeValueAsString(LocalDateTime.parse(input));
+
+        assertEquals("\"" + input + "\"", serialized);
+    }
+}
