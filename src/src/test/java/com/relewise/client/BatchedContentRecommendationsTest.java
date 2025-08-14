16de2bd0f363fa4d2dd2b77f66e56487e8a1d22c
Kristoffer Strube - Relewise <119578211+KSGRelewise@users.noreply.github.com>
2025-08-14 15:44:38 +0200

    Re-added test as it no longer fails.

--- src/src/test/java/com/relewise/client/BatchedContentRecommendationsTest.java
+++ src/src/test/java/com/relewise/client/BatchedContentRecommendationsTest.java
@@ -8,34 +8,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BatchedContentRecommendationsTest extends TestBase {
     @Test
-    public void testBatchedProductRecommendations() throws Exception {
-        // var recommender = new Recommender(GetDatasetId(), GetApiKey(), "https://api.relewise.com");
+    public void testBatchedContentRecommendations() throws Exception {
+        var recommender = new Recommender(GetDatasetId(), GetApiKey(), "https://api.relewise.com");
 
-        // var contentRecommendationRequestCollection  = ContentRecommendationRequestCollection.create(
-        //     false,
-        //     PopularContentsRequest.create(
-        //         Language.create("en-US"),
-        //         Currency.create("USD"),
-        //         "batched integration test",
-        //         UserFactory.byTemporaryId("t-Id"),
-        //         100000
-        //     ).setSettings(ContentRecommendationRequestSettings.create().setNumberOfRecommendations(1)),
-        //     ContentsViewedAfterViewingContentRequest.create(
-        //         Language.create("en-US"),
-        //         Currency.create("USD"),
-        //         "batched integration test",
-        //         UserFactory.byTemporaryId("t-Id"),
-        //         "1"
-        //     ).setSettings(ContentRecommendationRequestSettings.create().setNumberOfRecommendations(1))
-        // );
+        var contentRecommendationRequestCollection  = ContentRecommendationRequestCollection.create(
+            false,
+            PopularContentsRequest.create(
+                Language.create("en-US"),
+                Currency.create("USD"),
+                "batched integration test",
+                UserFactory.byTemporaryId("t-Id"),
+                100000
+            ).setSettings(ContentRecommendationRequestSettings.create().setNumberOfRecommendations(1)),
+            ContentsViewedAfterViewingContentRequest.create(
+                Language.create("en-US"),
+                Currency.create("USD"),
+                "batched integration test",
+                UserFactory.byTemporaryId("t-Id"),
+                "1"
+            ).setSettings(ContentRecommendationRequestSettings.create().setNumberOfRecommendations(1))
+        );
 
-        // var response = recommender.recommend(contentRecommendationRequestCollection );
+        var response = recommender.recommend(contentRecommendationRequestCollection );
 
-        // assertNotNull(response);
-        // assertEquals(2, response.responses.length);
-        // assertNotEquals(0, response.responses[0].recommendations.length);
-        // assertNotEquals(0, response.responses[1].recommendations.length);
-
-        assertTrue(true);
+        assertNotNull(response);
+        assertEquals(2, response.responses.length);
+        ssertNotEquals(0, response.responses[0].recommendations.length);
+        assertNotEquals(0, response.responses[1].recommendations.length);
     }
 }
